5b604dc9e78c2c892922062d291d73a8a6bb3224
CamiloVargas <camilovargasingm@gmail.com>
2022-01-15 12:25:41 -0500

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
-import './App.css'
+import './App.scss'
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>ToDo List</h1>
+        <p>Test tecnical of lya Electronic</p>
       </header>
     </div>
   )
